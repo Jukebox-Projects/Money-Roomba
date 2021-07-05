01dd46e12b6b75e8600d481b855e306c29931c64
Andres Bonilla <andres.bonilla1298@gmail.com>
2021-07-04 20:45:58 -0600

    Initial version of moneyRoomba generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/moneyroomba/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.moneyroomba.config;
---
+++ src/main/java/com/moneyroomba/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.moneyroomba.repository;
+
+import com.moneyroomba.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
