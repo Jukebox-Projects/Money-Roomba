2c1344532bb8a3b5a55aec8e00305f7c9259f8dc
ksaumaz <64628051+ksaumaz@users.noreply.github.com>
2021-07-17 23:25:04 -0600

    Sidebar funcionando

--- src/main/webapp/app/template/elements/navigation/navigation.component.ts
+++ src/main/webapp/app/template/elements/navigation/navigation.component.ts
@@ -1,7 +1,15 @@
+import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { AccountService } from 'app/core/auth/account.service';
+import { SessionStorageService } from 'ngx-webstorage';
+import { TranslateService } from '@ngx-translate/core';
+import { LoginService } from 'app/login/login.service';
+import { Account } from 'app/core/auth/account.model';
+import { LANGUAGES } from 'app/config/language.constants';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
+import { VERSION } from 'app/app.constants';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -9,8 +17,25 @@ import { Router } from '@angular/router';
 })
 export class NavigationComponent implements OnInit {
   public currentHref: string = '';
+  inProduction?: boolean;
+  isNavbarCollapsed = true;
+  languages = LANGUAGES;
+  openAPIEnabled?: boolean;
+  version = '';
+  account: Account | null = null;
 
-  constructor(location: Location, router: Router) {
+  constructor(
+    location: Location,
+    router: Router,
+    private loginService: LoginService,
+    private translateService: TranslateService,
+    private sessionStorageService: SessionStorageService,
+    private accountService: AccountService,
+    private profileService: ProfileService
+  ) {
+    if (VERSION) {
+      this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION;
+    }
     router.events.subscribe(val => {
       if (location.path() != '') {
         this.currentHref = location.path();
@@ -20,7 +45,13 @@ export class NavigationComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.profileService.getProfileInfo().subscribe(profileInfo => {
+      this.inProduction = profileInfo.inProduction;
+      this.openAPIEnabled = profileInfo.openAPIEnabled;
+    });
+    this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+  }
 
   toggleIcon: boolean = true;
 
@@ -28,119 +59,15 @@ export class NavigationComponent implements OnInit {
     this.toggleIcon = !this.toggleIcon;
   }
 
-  dashboardArray = [
-    '/admin',
-    '/admin/index',
-    '/admin/my-wallet',
-    '/admin/invoices',
-    '/admin/cards-center',
-    '/admin/transactions',
-    '/admin/transactions-details',
+  dashboardArray = ['/', '/wallet', '/scheduled-transaction', '/category'];
+
+  adminArray = [
+    '/admin/user-management',
+    '/admin/metrics',
+    '/admin/health',
+    '/admin/logs',
+    '/admin/docs',
+    '/admin/configuration',
+    '/system-setting',
   ];
-
-  apsArray = [
-    '/admin/app-profile',
-    '/admin/post-details',
-    '/admin/email-compose',
-    '/admin/email-inbox',
-    '/admin/email-read',
-    '/admin/app-calender',
-    '/admin/ecom-product-grid',
-    '/admin/ecom-product-list',
-    '/admin/ecom-product-detail',
-    '/admin/ecom-product-order',
-    '/admin/ecom-checkout',
-    '/admin/ecom-invoice',
-    '/admin/ecom-customers',
-  ];
-
-  chartsArray = [
-    '/admin/chart-chartjs',
-    '/admin/chart-apex',
-    '/admin/apex-line',
-    '/admin/apex-area',
-    '/admin/apex-column',
-    '/admin/apex-bar',
-    '/admin/apex-mixed',
-    '/admin/apex-timeline',
-    '/admin/apex-candlestick',
-    '/admin/apex-pie',
-    '/admin/apex-radar',
-    '/admin/apex-radialbar',
-    '/admin/apex-polar-area',
-    '/admin/apex-bubble',
-    '/admin/apex-scatter',
-    '/admin/apex-heatmap',
-    '/admin/apex-treemap',
-    '/admin/apex-sparklines',
-  ];
-
-  bootstrapArray = [
-    '/admin/ui-accordion',
-    '/admin/ui-alert',
-    '/admin/ui-badge',
-    '/admin/ui-button',
-    '/admin/ui-datepicker',
-    '/admin/ui-modal',
-    '/admin/ui-button-group',
-    '/admin/ui-list-group',
-    '/admin/ui-media-object',
-    '/admin/ui-card',
-    '/admin/ui-carousel',
-    '/admin/ui-dropdown',
-    '/admin/ui-popover',
-    '/admin/ui-progressbar',
-    '/admin/ui-nav',
-    '/admin/ui-rating',
-    '/admin/ui-typography',
-    '/admin/ui-table',
-    '/admin/ui-pagination',
-    '/admin/ui-timepicker',
-    '/admin/ui-toast',
-    '/admin/ui-tooltip',
-    '/admin/ui-typeahead',
-    '/admin/ui-grid',
-  ];
-
-  materialArray = [
-    '/admin/mat-autocomplete',
-    '/admin/mat-badge',
-    '/admin/mat-bottom-sheet',
-    '/admin/mat-button',
-    '/admin/mat-button-toggle',
-    '/admin/mat-card',
-    '/admin/mat-checkbox',
-    '/admin/mat-chips',
-    '/admin/mat-datepicker',
-    '/admin/mat-dialog',
-    '/admin/mat-divider',
-    '/admin/mat-expansion',
-    '/admin/mat-form-field',
-    '/admin/mat-grid-list',
-    '/admin/mat-icon',
-    '/admin/mat-input',
-    '/admin/mat-list',
-    '/admin/mat-menu',
-    '/admin/mat-paginator',
-    '/admin/mat-progress-bar',
-    '/admin/mat-progress-spinner',
-    '/admin/mat-radio',
-    '/admin/mat-ripple',
-    '/admin/mat-select',
-    '/admin/mat-sidenav',
-    '/admin/mat-slide-toggle',
-    '/admin/mat-slider',
-    '/admin/mat-snack-bar',
-    '/admin/mat-sort',
-    '/admin/mat-stepper',
-    '/admin/mat-table',
-    '/admin/mat-tab',
-    '/admin/mat-tooltip',
-    '/admin/mat-tree',
-    '/admin/mat-toolbar',
-  ];
-
-  pluginsArray = ['/admin/uc-nestable', '/admin/uc-lightgallery'];
-
-  formsArray = ['/admin/form-element', '/admin/form-validate'];
 }
