c6a05147c408ace7f4894931c638fc34415146fd
Javier Pozuelo <38415516+JPozueloM@users.noreply.github.com>
2021-07-27 02:56:47 -0600

    New landing page

--- src/main/webapp/app/landing/landing-page/landing-page.component.ts
+++ src/main/webapp/app/landing/landing-page/landing-page.component.ts
@@ -3,15 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jhi-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['../../../assets/landing/styles/styles.css'],
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
-  myScriptElement: HTMLScriptElement;
-  constructor() {
-    this.myScriptElement = document.createElement('script');
-    this.myScriptElement.src = '../../../assets/landing/scripts/main.js';
-    document.body.appendChild(this.myScriptElement);
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
