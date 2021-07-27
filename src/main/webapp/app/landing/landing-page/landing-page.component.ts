e69ed19cde09622e8a062eb045e7afa7c8756da4
Andres Bonilla <abonillaa@ucenfotec.ac.cr>
2021-07-27 01:43:57 -0600

    posibles modificaciones al landing para el deploy

--- src/main/webapp/app/landing/landing-page/landing-page.component.ts
+++ src/main/webapp/app/landing/landing-page/landing-page.component.ts
@@ -3,15 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jhi-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['../../../assets/landing/styles/styles.css'],
 })
 export class LandingPageComponent implements OnInit {
-  myScriptElement: HTMLScriptElement;
-  constructor() {
-    this.myScriptElement = document.createElement('script');
-    this.myScriptElement.src = '../../../assets/landing/scripts/main.js';
-    document.body.appendChild(this.myScriptElement);
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
