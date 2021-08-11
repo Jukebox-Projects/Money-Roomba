5a3a3ceb36480da7834035d2abf26a5b2f45a003
ksaumaz <64628051+ksaumaz@users.noreply.github.com>
2021-08-11 00:36:01 -0600

    Precio de licencia en landing page, además se abre el api de system-settings para que pueda ser visto por todos sin necesidad de login

--- src/main/webapp/app/landing/landing-page/landing-page.component.ts
+++ src/main/webapp/app/landing/landing-page/landing-page.component.ts
@@ -1,3 +1,6 @@
+import { HttpResponse } from '@angular/common/http';
+import { ISystemSetting } from './../../entities/system-setting/system-setting.model';
+import { SystemSettingService } from './../../entities/system-setting/service/system-setting.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
-  constructor() {}
+  constructor(protected systemSettingService: SystemSettingService) {}
+  systemSettings?: ISystemSetting[];
+  isLoading = false;
+  price = 0;
+  tax = 0.0;
+  loadAll(): void {
+    this.isLoading = true;
+    this.systemSettingService.query().subscribe(
+      (res: HttpResponse<ISystemSetting[]>) => {
+        this.isLoading = false;
+        this.systemSettings = res.body ?? [];
+        this.price = this.systemSettings?.find(kv => kv?.key === 'price')?.value || 0;
+        this.tax = this.systemSettings?.find(kv => kv?.key === 'tax')?.value || 0;
+      },
+      () => {
+        this.isLoading = false;
+      }
+    );
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadAll();
+  }
 }
